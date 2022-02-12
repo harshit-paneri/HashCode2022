a38ae4cdb51582dba316052745bbe0b9cedf86bf
Saurav_Sutaria <sauravpatel002@gmail.com>
2022-02-12 16:32:44 +0530

    code

--- code.cpp
+++ code.cpp
@@ -6,86 +6,131 @@ vector<vector<string>> like;
 vector<vector<string>> dislike;
 int maxCust = 0;
 unordered_set<string> finalIngri;
-void simulator(unordered_set<string> total,int cust){
-    int count = 0;    
+void subsetsUtil(int n, vector<vector<int>> &res, vector<int> &subset, int index)
+{
+    res.push_back(subset);
+    for (int i = index; i < n; i++)
+    {
+
+        // include the A[i] in subset.
+        subset.push_back(i);
+
+        // move onto the next element.
+        subsetsUtil(n, res, subset, i + 1);
+
+        // exclude the A[i] from subset and triggers
+        // backtracking.
+        subset.pop_back();
+    }
+    return;
+}
+vector<vector<int>> subsets(int n)
+{
+    vector<int> subset;
+    vector<vector<int>> res;
+
+    // keeps track of current element in vector A;
+    int index = 0;
+    subsetsUtil(n, res, subset, index);
+
+    return res;
+}
+
+void simulator(unordered_set<string> total, int cust)
+{
+    int count = 0;
     int flag;
-    for(int i=0;i<cust;i++){
+    for (int i = 0; i < cust; i++)
+    {
         flag = 1;
-        //like check
-        for(int j=0;j<like[i].size();j++){
-            if(total.find(like[i][j]) == total.end()){
+        // like check
+        for (int j = 0; j < like[i].size(); j++)
+        {
+            if (total.find(like[i][j]) == total.end())
+            {
                 flag = 0;
                 break;
             }
         }
-        //dislike check
-        if(flag){
-            for(int j=0;j<dislike[i].size();j++){
-                if(total.find(dislike[i][j]) != total.end()){
+        // dislike check
+        if (flag)
+        {
+            for (int j = 0; j < dislike[i].size(); j++)
+            {
+                if (total.find(dislike[i][j]) != total.end())
+                {
                     flag = 0;
                     break;
                 }
             }
         }
-        if(flag) count++;
+        if (flag)
+            count++;
     }
-    if(count > maxCust){
+    if (count > maxCust)
+    {
         finalIngri = total;
         maxCust = count;
-    }  
-}
-void removeEle(vector<vector<string>> dislike,int i,unordered_set<string> total,int cust){
-    for(auto it:dislike[i]){
-        total.erase(it);
     }
-    simulator(total,cust);
 }
-
+void removeEle(vector<vector<string>> dislike, vector<int> idx, unordered_set<string> total, int cust)
+{
+    //{0,1}
+    for (int i = 0; i < idx.size(); i++)
+    {
+        for (auto it : dislike[i])
+        {
+            total.erase(it);
+        }
+    }
+    simulator(total, cust);
+}
 
 int main()
-{   
+{
     long long cust;
     cin >> cust;
     unordered_set<string> total;
     /*
     1 : like :- a,b,c dislike :- d,e
     2 : like :- b,c,d dislike :- f
+    {{d,e},{f}}
     total : a,b,c,f
     */
     for (int c = 0; c < cust; c++)
     {
         int l;
-        cin >> l;//like input
+        cin >> l; // like input
         vector<string> temp;
         while (l--)
         {
             string s;
             cin >> s;
             temp.push_back(s);
-            total.insert(s);            
+            total.insert(s);
         }
         like.push_back(temp);
         vector<string> temp1;
         int d;
-        cin >> d;//dislike input
+        cin >> d; // dislike input
         while (d--)
         {
             string s;
             cin >> s;
             temp1.push_back(s);
-            total.insert(s);  
-            
+            total.insert(s);
         }
         dislike.push_back(temp1);
     }
-    for(int i=0;i<cust;i++){
-        removeEle(dislike,i,total,cust);
-    }  
-    cout<<maxCust<<endl;
-    cout<<finalIngri.size()<<" ";
-    for(auto it:finalIngri) cout<<it<<" "; 
-
-    
+    vector<vector<int>> totalSubset = subsets(cust);
+    for (int i = 0; i < totalSubset.size(); i++)
+    {
+        removeEle(dislike, totalSubset[i], total, cust);
+    }
+    cout << maxCust << endl;
+    cout << finalIngri.size() << " ";
+    for (auto it : finalIngri)
+        cout << it << " ";
 
     return 0;
 }
